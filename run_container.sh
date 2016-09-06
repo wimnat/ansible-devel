5c8461d60b20194c6e7ede69cd2404583796b832
Rob White <rob.white@truelocal.com.au>
2016-09-06 15:09:17 +1000

    Initial commit

---
+++ run_container.sh
@@ -0,0 +1,1 @@
+docker run -v .:/opt -i -t centos /bin/bash
---
+++ setup_container.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+yum -y install python-setuptools epel-release
+yum -y install ansible python-pip
+yum -y remove ansible
+pip install boto boto3
+ln -s /opt/ansible/hacking/test-module /usr/bin/test-module
+mkdir -p ~/.aws
+touch ~/.aws/credentials
+cp aws_credentials_template ~/.aws/credentials
+
