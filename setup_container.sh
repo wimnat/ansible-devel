c1d3bf33c88d02eb725bbaefb386a77b739359e8
Rob White <rob.white@truelocal.com.au>
2016-11-30 10:04:15 +1100

    Add python_bin env variable

--- setup_container.sh
+++ setup_container.sh
@@ -8,4 +8,4 @@ ln -s /opt/ansible/hacking/test-module /usr/bin/test-module
 mkdir -p ~/.aws
 touch ~/.aws/credentials
 cp aws_credentials_template ~/.aws/credentials
-
+export PYTHON_BIN=/usr/bin/python
